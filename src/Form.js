7ecc852b5e427e6b0a26b20b7a51855a9b993f6d
Emilian Murawski <murawski.emilian@gmail.com>
2023-08-05 21:06:42 +0200

    style change 'add equipment' button color

--- src/Form.js
+++ src/Form.js
@@ -142,7 +142,7 @@ export const Form = () => {
 
                 <button
                     type="button"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2"
                     onClick={() =>
                         formik.setFieldValue("equipments", [
                             ...formik.values.equipments,
