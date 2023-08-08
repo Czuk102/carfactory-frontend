ac8505e490cb9e1d7dfec3a4a9648fe78f3bb67b
Emilian Murawski <murawski.emilian@gmail.com>
2023-08-08 23:01:23 +0200

    refactor api calls in separate file

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,7 @@
 export async function getCars(){
     const response = await fetch("http://localhost:8080/cars/all");
     if (!response.ok){
-        throw {message: "ed to fetch cars", status: 500};
+
     }
     return response.json();
 }
