fd74f5c14846698cd1fc552f0d40d46eca17566d
Emilian Murawski <murawski.emilian@gmail.com>
2023-08-17 23:49:57 +0200

    feat current location header corresponding to location

--- src/Layout.js
+++ src/Layout.js
@@ -40,6 +40,7 @@ function Layout() {
 
         setUpdatedNavigation(updatedNav);
     }, [location]);
+    const currentNavigationItem = updatedNavigation.find((item) => item.current);
     return (
 
     <>
@@ -188,7 +189,7 @@ function Layout() {
 
             <header className="bg-white shadow">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">{currentNavigationItem ? currentNavigationItem.name : ""}</h1>
                 </div>
             </header>
             <main>
