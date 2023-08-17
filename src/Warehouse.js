2fd393bd3fb877abfd9f8bb27c3cbc58a9824440
Emilian Murawski <murawski.emilian@gmail.com>
2023-08-17 23:51:21 +0200

    feat delete car from warehouse feature

--- src/Warehouse.js
+++ src/Warehouse.js
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import Cars from "./Cars";
-import {getCars} from "./util/api";
+import {deleteCarById, getCars} from "./util/api";
 import {useLoaderData} from "react-router-dom";
 
 export default function Warehouse() {
-    const loaderData = useLoaderData()
-    
+    const loaderData = useLoaderData();
+    const [carList, setCarList] = useState(loaderData || []);
+
+    useEffect(() => {
+        if (!loaderData) {
+            getCars()
+                .then((data) => setCarList(data.results))
+                .catch((error) => console.error("Failed to fetch cars:", error));
+        }
+    }, [loaderData]);
+
+    const handleDeleteCar = (id) => {
+        deleteCarById(id)
+            .then(() => {
+
+                setCarList((prevCarList) => prevCarList.filter((car) => car.id !== id));
+            })
+            .catch((error) => console.error("Failed to delete car:", error));
+    };
+
     return (
-        <Cars carList={loaderData}/>
+        <Cars onDeleteCar={handleDeleteCar} carList={carList}/>
     );
 };
 export function loader(){
