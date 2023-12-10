42eed23613bd372d1e70ec2b44b930d701e0f6ba
Emilian Murawski <murawski.emilian@gmail.com>
2023-12-10 20:46:22 +0100

    feat add login request and minor refac

--- src/util/api.js
+++ src/util/api.js
@@ -1,11 +1,37 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080";
-export async function getCars(){
-    const response = await axios.get(`${API_URL}/cars/all`);
+
+export async function getCars() {
+    const response = await axios.get(`${API_URL}/cars/all`, {
+        headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+        }
+    });
 
     return response.data;
 }
-export async function  deleteCarById(id){
-    await axios.delete(`${API_URL}/cars/${id}`);
+
+export async function deleteCarById(id) {
+    await axios.delete(`${API_URL}/cars/${id}`, {
+        headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+        }
+    });
+}
+
+export async function login(username, password) {
+    const response = await axios.post(`${API_URL}/token`, {}, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        auth: {
+            username,
+            password
+        }
+    });
+
+    return response;
 }
